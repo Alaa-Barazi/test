46df03d40787ebf09d38cb6bcc4d57c3814de1ab
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-03-13 10:27:15 +0200

    first commit

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 // git init
 // git add README.md
 // git commit -m "first commit"
-// git branch -M main
+// git branch -M main2
 // git remote add origin https://github.com/Alaa-Barazi/php.git
-// git push -u origin main
+// git push -u origin main2
